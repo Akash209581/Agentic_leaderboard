03ac52f07d6189b2f497adc791e92f248297de5c
Akash209581 <bandaruakash8@gmail.com>
2026-08-27 22:15:19 +0530

    source map false

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/leaderboard/',
   plugins: [react()],
+  build: {
+    sourcemap: false
+  },
   server: {
     proxy: {
       '/api': {
